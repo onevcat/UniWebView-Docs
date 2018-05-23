e3aec43f139b78db549eebd39fb083bb1b4b050b
onevcat <onevcat@gmail.com>
2018-05-23 10:20:46 +0900

    Update build script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-yarn build
 cp ../UniWebView/Assets/UniWebView/CHANGELOG.md ./docs/release-note/README.md
+yarn build
 rsync -auv dist onevcat@104.236.137.50:/home/onevcat/uniwebview-docs/
