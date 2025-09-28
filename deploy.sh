4ff4d61a7885fc020b50947fc9518e759f82160c
onevcat <onevcat@gmail.com>
2025-09-28 11:55:35 +0900

    v5 doc update target

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 cp ../UniWebView/Assets/UniWebView/CHANGELOG.md ./docs/release-note/README.md
 yarn build
-rsync -auv dist onevcat@uniwebview.com:/home/onevcat/www/uniwebview-docs/
+rsync -auv dist onevcat@uniwebview.com:/home/onevcat/www/uniwebview-docs-v5/
